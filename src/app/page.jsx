0e7e8184fc7bee25d027ebad4d0b5fb75e86354d
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-02-09 22:20:24 +0530

    COM : starter commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mb-2 mt-3 flex items-center justify-center py-2">
+      <h1>Welcome to the next js starter.</h1>
+    </div>
+  );
+}
